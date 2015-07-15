94e067222ddd6554cd82b35039c612563c156619
Leonardo Herbert <leonardo@rarolabs.com.br>
2015-07-15 16:40:58 -0300

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { :host => "http://benevole.rarolabs.com.br" }
+  config.action_controller.asset_host = "http://benevole.rarolabs.com.br"
+  
 end
