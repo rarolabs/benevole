36c97b190cc0689136da271e0ae28ced50679472
Leonardo Herbert <leonardo@rarolabs.com.br>
2015-07-23 09:20:18 -0300

    fix veiculos

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -4,15 +4,7 @@ class Veiculo < ActiveRecord::Base
   def self.outros
     find_by(nome: "Outros") || []
   end
-  
-  def self.com_acesso
-    if Usuario.current.present?
-      Veiculo.accessible_by(Usuario.current.ability)
-    else
-      []
-    end
-  end
-  
+
   def to_s
     nome
   end
